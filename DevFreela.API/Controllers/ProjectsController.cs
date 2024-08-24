07182c2effd75f087f92703c81cf29b0fca2f9e6
herondi <wayneramos93@gmail.com>
2024-08-24 11:30:06 -0300

    Criando rotas do Endpoint API

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -24,6 +24,44 @@ namespace DevFreela.API.Controllers
         [HttpPost]
         public IActionResult Post(CreateProjectInputModel model)
         {
+            return CreatedAtAction(nameof(GetById), new { id = 1 }, model);
+        }
+
+        // PUT api/projects/1234
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, UpdateProjectInputModel model) 
+        {
+        return NoContent();
+        }
+
+        // DELETE api/projects/1234
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+        return NoContent();
+        }
+
+        // PUT api/projects/1234/start
+        [HttpPut("{id}/start")]
+
+        public IActionResult Start(int id) 
+        {
+            return NoContent();
+        }
+
+        // PUT api/projects/1234/complete
+        [HttpPut("{id}/complete")]
+
+        public IActionResult Complete(int id)
+        {
+            return NoContent();
+        }
+
+        // POST api/projects/1234/comments
+        [HttpPost("{id}/comments")]
+
+        public IActionResult PostComment(int id, CreateProjectCommentInputModel model)
+        {
             return Ok();
         }
     }
